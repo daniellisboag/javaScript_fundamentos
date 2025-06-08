6df96455e03b32de055ace711e43f1bce9a0fac8
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-06-08 19:22:43 -0300

    Tipos boolenaos

---
+++ boolean.js
@@ -0,0 +1,14 @@
+const estudante = 'Daniel';
+const estaAprovado = true;
+
+if (estaAprovado === true) {
+   console.log('Parabéns, boas festas.');
+} else {
+   console.log('Reprovado, boas festas.');
+}
+
+if (estudante === 'Daniel') {
+   console.log(`Olá, ${estudante}`);
+} else {
+   console.log('Quem é você?');
+}
