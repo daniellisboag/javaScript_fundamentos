c2e69c3bf35b75380f9b144e4318a017c798b718
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-06-09 20:54:45 -0300

    Operadores lógicos

--- operadoresLogicos.js
+++ operadoresLogicos.js
@@ -1,5 +1,6 @@
 const notaFinal = 7;
-const faltas = 3;
+const faltas = 2;
+const advertencias = 0;
 
 if (notaFinal < 7 || faltas > 4) {
    console.log('Reprovado, boas festas.');
@@ -11,4 +12,10 @@ if (notaFinal < 7 && faltas > 4) {
    console.log('Reprovado, boas festas.');
 } else {
    console.log('Aprovado, boas festas.');
+}
+
+if (faltas <= 2 && !advertencias) {
+   console.log('Recebeu bônus.');
+} else {
+   console.log('Não recebeu bônus.');
 }
