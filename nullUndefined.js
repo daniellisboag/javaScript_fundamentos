10da4f1856d61cca106ecf74062abbba74a3b9f6
Daniel Lisboa Gonçalves <daniellisboag0@gmail.com>
2025-06-08 21:04:27 -0300

    Tipos null e undefined

---
+++ nullUndefined.js
@@ -0,0 +1,8 @@
+let nomeEstudante;
+const professor = 'Daniel';
+console.log(nomeEstudante);
+console.log(typeof nomeEstudante);
+console.log(typeof professor);
+
+let telefoneEstudante = null;
+console.log(typeof telefoneEstudante);
